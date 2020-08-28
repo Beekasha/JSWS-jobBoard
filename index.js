3d8d5f26941e512357bfadcae56ff402c1d7a3e2
Beekasha <brian.j.kleiner@gmail.com>
2020-08-28 05:34:59 -0400

    SHEET_ID_FROM_URL saved in environment variable

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
+require('dotenv').config()
 const { GoogleSpreadsheet } = require('google-spreadsheet');
+
  
 // spreadsheet key is the long id in the sheets URL
-const doc = new GoogleSpreadsheet('<the sheet ID from the url>');
+const doc = new GoogleSpreadsheet(process.env.SHEET_ID_FROM_URL);
  
-// use service account creds
-await doc.useServiceAccountAuth({
-  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-  private_key: process.env.GOOGLE_PRIVATE_KEY,
-});
-// OR load directly from json file if not in secure environment
-await doc.useServiceAccountAuth(require('./creds-from-google.json'));
-// OR use service account to impersonate a user (see https://developers.google.com/identity/protocols/oauth2/service-account#delegatingauthority)
-await doc.useServiceAccountAuth(require('./creds-from-google.json'), 'some-user@my-domain.com');
-// OR use API key -- only for read-only access to public sheets
-doc.useApiKey('YOUR-API-KEY');
- 
-await doc.loadInfo(); // loads document properties and worksheets
-console.log(doc.title);
-await doc.updateProperties({ title: 'renamed doc' });
- 
-const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-console.log(sheet.title);
-console.log(sheet.rowCount);
- 
-// adding / removing sheets
-const newSheet = await doc.addSheet({ title: 'hot new sheet!' });
-await newSheet.delete();
+(async function() {
+
+
+
+    // OR load directly from json file if not in secure environment
+    await doc.useServiceAccountAuth(require('./credentials.json'));
+
+    await doc.loadInfo(); // loads document properties and worksheets
+    console.log(doc.title);
+    await doc.updateProperties({ title: 'renamed doc2' });
+    
+    // const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
+    // console.log(sheet.title);
+    // console.log(sheet.rowCount);
+    
+    // // adding / removing sheets
+    // const newSheet = await doc.addSheet({ title: 'hot new sheet!' });
+    // await newSheet.delete();
+})()
