19082ae7ffd8edfb055d3d4a139c818b288b8720
Beekasha <brian.j.kleiner@gmail.com>
2020-09-09 15:59:56 -0400

    forgot to save

--- sheet.js
+++ sheet.js
@@ -1,21 +1,7 @@
 require('dotenv').config()
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
-// module.exports = class Sheet {
-//     constructor() {
-//         this.doc = new GoogleSpreadsheet(process.env.SHEET_ID_FROM_URL);
-//     }
-//     async load() {
-//         await this.doc.useServiceAccountAuth(require('./credentials.json'));
-//         await this.doc.loadInfo(); // loads document properties and worksheets
-//     }
-//     async addRows(rows) {
-//         const sheet = this.doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-//         await sheet.addRows(rows);
-//     }
-// }
-
-class Sheet {
+module.exports = class Sheet {
     constructor() {
         this.doc = new GoogleSpreadsheet(process.env.SHEET_ID_FROM_URL);
     }
@@ -29,13 +15,14 @@ class Sheet {
     }
 }
 
-(async function() {
-    const sheet = new Sheet();
-    await sheet.load();
-    sheet.addRows([
-        { title: 'Software Engineeeeeeeeeeeeeeeeer', location: 'FL' },
-        { title: 'Software Developer', location: 'LA' },
-    ])
+// // Test Function
+// (async function() {
+//     const sheet = new Sheet();
+//     await sheet.load();
+//     sheet.addRows([
+//         { title: 'Software Engineeeeeeeeeeeeeeeeer', location: 'FL' },
+//         { title: 'Software Developer', location: 'LA' },
+//     ])
     
-})()
+// })()
 
