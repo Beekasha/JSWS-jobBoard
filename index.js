a577bec65a0a1608c64d43ae048fa62973f501b1
Beekasha <brian.j.kleiner@gmail.com>
2020-09-09 16:11:20 -0400

    made changes

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ const doc = new GoogleSpreadsheet(process.env.SHEET_ID_FROM_URL);
  
 (async function() {
 
-
-
-    // OR load directly from json file if not in secure environment
-    await doc.useServiceAccountAuth(require('./credentials.json'));
-
-    await doc.loadInfo(); // loads document properties and worksheets
-    console.log(doc.title);
     await doc.updateProperties({ title: 'renamed secure' });
     
     const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
@@ -21,10 +14,4 @@ const doc = new GoogleSpreadsheet(process.env.SHEET_ID_FROM_URL);
         { title: 'Software Engineer', location: 'FL' },
         { title: 'Software Developer', location: 'LA' },
     ]);
-    // console.log(sheet.title);
-    // console.log(sheet.rowCount);
-    
-    // // adding / removing sheets
-    // const newSheet = await doc.addSheet({ title: 'hot new sheet!' });
-    // await newSheet.delete();
 })()
