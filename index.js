18fe2ad9b7dfb8df558524a1eb10685efb42d376
Beekasha <brian.j.kleiner@gmail.com>
2020-09-09 23:35:21 -0400

    console.logging remote jobs json

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fetch = require('node-fetch');
     const sheet = new Sheet();
     await sheet.load();
 
-    const resp = await fetch('https://jobs.github.com/positions.json?description=python&location=new+york');
+    const resp = await fetch('https://jobs.github.com/positions.json?description=python&location=remote');
     const json = await resp.json();
 
     console.log({json})
